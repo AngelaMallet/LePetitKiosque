3d979809c6f80a7d191fae1793afe09e43557993
Angela Mallet <angelamallet@gmail.com>
2019-02-04 22:30:08 +0100

    Modification du formulaire d'incription

--- form.php
+++ form.php
@@ -1,47 +1,60 @@
-
+  <div class="container">  
 <h1 class="formTitle center-align red-text text-red accent-1">Formulaire d'inscription</h1>
-<div class="container center-align">
-	<div class="row">
-		<form id="form" method="POST" class="col s12 l12" action="index.php">
-			<div class="row input-field col s12 l6">
-				<input name="fistName" value ="<?= isset($name) ? $name : ''; ?>" id ="fistName" type="text" class="active" />
-				<label for="name">Prénom</label>
-				<p class="css" id="firstNameField"><?= isset($formError['fistName']) ? $formError['fistName'] : '' ?></p>
-			</div>
-			<div class="row input-field col s12 l6">
-				<input name="lastName" value = "<?= isset($lastName) ? $lastName : ''; ?>" id = "lastName" type="text" class = "active" />
-				<label for="lastName">Nom</label>
-				<p class="css" id="lastNameField"><?= isset($formError['lastName']) ? $formError['lastName'] : '' ?></p>
-			</div>
-			<div class="row input-field col s12 l6">
-				<input name="email" value="<?= isset($email) ? $email : ''; ?>" id="email" type="email" class="active">
-				<label for="email">Email</label>
-				<p class="css" id="emailNameField"><?= isset($formError['email']) ? $formError['email'] : '' ?></p>
-			</div>
-			<div class="row">
-				<div class="input-field col s12 l6">
-					<input name="password" value="<?= isset($password) ? $password : ''; ?>" id="password" type="password" class="active">
-					<label for="password">Mot de passe</label>
-					<p class="css" id="passwordField"><?= isset($formError['password']) ? $formError['password'] : '' ?></p>
-				</div>
-			</div>
-                    <div class="row input-field col s12 l6">
-				<input name="tel" value="<?= isset($tel) ? $tel : ''; ?>" id="tel" type="tel" class="active">
-				<label for="tel">N° de téléphone</label>
-				<p class="css" id="telNameField"><?= isset($formError['tel']) ? $formError['tel'] : '' ?></p>
-			</div>
-                    <div class="row input-field col s12 l6">
-				<input name="address" value="<?= isset($address) ? $address : ''; ?>" id="address" type="address" class="active">
-				<label for="address">Votre adresse</label>
-				<p class="css" id="addressNameField"><?= isset($formError['address']) ? $formError['address'] : '' ?></p>
-			</div>
-			<div class="row input-field col s12 l12">
-				<button class="modal-close waves-effect waves-light btn red accent-1 modal-trigger" type="submit" name="submitForm">M'inscrire</button>
-                            <button><a href="accountuser.php" class="modal-close waves-effect waves-light btn red accent-2 modal-trigger">S'identifier</a></button>
-			</div>
-			<!-- ... <p><a id="linkForm" href="index.php">Vous êtes déjà inscrit?</a></p>-->
-		</form>
-	</div>
 
-</div>
+    <div class="row">
+        <form id="form" method="POST" class="col s12 offset-l2" action="index.php">
+            <div class="row input-field col s12 l12">
+                <input name="fistName" value ="<?= isset($name) ? $name : ''; ?>" id ="fistName" type="text" class="active" />
+                <label for="name">Prénom*</label>
+                <p class="css" id="firstNameField"><?= isset($formError['fistName']) ? $formError['fistName'] : '' ?></p>
+            </div>
+            <div class="row input-field col s12 l12">
+                <input name="lastName" value = "<?= isset($lastName) ? $lastName : ''; ?>" id = "lastName" type="text" class = "active" />
+                <label for="lastName">Nom*</label>
+                <p class="css" id="lastNameField"><?= isset($formError['lastName']) ? $formError['lastName'] : '' ?></p>
+            </div>
+            <div class="row input-field col s12 l12">
+                <input name="email" value="<?= isset($email) ? $email : ''; ?>" id="email" type="email" class="active">
+                <label for="email">Email*</label>
+                <p class="css" id="emailNameField"><?= isset($formError['email']) ? $formError['email'] : '' ?></p>
+            </div>
+            <div class="row">
+                <div class="input-field col s12 l12">
+                    <input name="password" value="<?= isset($password) ? $password : ''; ?>" id="password" type="password" class="active">
+                    <label for="password">Mot de passe*</label>
+                    <p class="css" id="passwordField"><?= isset($formError['password']) ? $formError['password'] : '' ?></p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12 l12">
+                    <input name="passwordConf" value="<?= isset($passwordConf) ? $passwordConf : ''; ?>" id="passwordConf" type="password" class="active">
+                    <label for="passwordConf">Confirmation du mot de passe*</label>
+                    <p class="css" id="passwordConfField"><?= isset($formError['passwordConf']) ? $formError['passwordConf'] : '' ?></p>
+                </div>
+            </div>
+            <div class="row input-field col s12 l12">
+                <input name="tel" value="<?= isset($tel) ? $tel : ''; ?>" id="tel" type="tel" class="active">
+                <label for="tel">N° de téléphone*</label>
+                <p class="css" id="telNameField"><?= isset($formError['tel']) ? $formError['tel'] : '' ?></p>
+            </div>
+            <div class="row input-field col s12 l12">
+                <input name="address" value="<?= isset($address) ? $address : ''; ?>" id="address" type="text" class="active">
+                <label for="address">Votre adresse</label>
+                <p class="css" id="addressNameField"><?= isset($formError['address']) ? $formError['address'] : '' ?></p>
+            </div>
+            <div class="row input-field col s12 l12">
+                <button class="modal-close waves-effect waves-light btn red accent-1 modal-trigger" type="submit" name="submitForm">M'inscrire</button>
+            </div>
+            
+            <div class="row">
+                <div class="col s12 l12">
+                    <h6>Vous êtes déjà inscrit ? </h6><a href="user.php">S'identifier</a>
+                </div>    
+            </div>
+        </form>
+    </div>
      
+   <!-- ... <p><a id="linkForm" href="index.php">Vous êtes déjà inscrit?</a></p>-->
+</div>
+    
+    
--- user.php
+++ user.php
@@ -1,25 +1,57 @@
-<h1 class="formTitle center-align red-text text-red accent-1">Votre compte</h1>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <!--Import Google Font-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,400i,600,700,800" rel="stylesheet" />
+        <!-- Favicons-->
+        <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+            
+        <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="animate.min.css">
+        <link rel="stylesheet" href="assets/css/style.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
+        <meta charset="utf-8" />
+        <title>CauxLibri</title>
+    </head>
+    <body>
+        
+        <header>
+            <!-- header et navbar -->
+            <?php include 'nav.html'; ?>
+        </header>
+        <h1 class="formTitle center-align red-text text-red accent-1">Votre compte</h1>
         <div class="container center-align">
             <div class="row">
-                <form id="formUser" method="POST" class="col s12 l12" action="index.php" >
+                <form id="formUser" method="POST" class="col s12 offset-l2 l8 offset-l2 " action="index.php" >
                     <div class="row input-field col s12 l6">
                         <input name="confimLogin" value = "" id = "confimLogin" type="text" class = "active" />
-                        <label for="confimLogin">Login</label>
+                        <label for="confimLogin">Email</label>
                         <p class="css" id="confimLoginField"><?= isset($formError['confimEmail']) ? $formError['confimEmail'] : '' ?></p>
                     </div>
-                     <div class="row">
-                            <div class="input-field col s12 l6">
-                                <input name="confirmPassword" value="" id="confirmPassword" type="password" class="active">
-                                <label for="confirmPassword">Mot de passe</label>
-                               <p class="css" id="confirmPasswordField"><?= isset($formError['confirmPassword']) ? $formError['confirmPassword'] : '' ?></p>
-                            </div>
+                    <div class="row">
+                        <div class="input-field col s12 l6">
+                            <input name="confirmPassword" value="" id="confirmPassword" type="password" class="active">
+                            <label for="confirmPassword">Mot de passe</label>
+                            <p class="css" id="confirmPasswordField"><?= isset($formError['confirmPassword']) ? $formError['confirmPassword'] : '' ?></p>
                         </div>
-
+                    </div>
+                        
                     <div class="row input-field col s12 l12">
-                        <button class="modal-close waves-effect waves-light btn red accent-1 modal-trigger" type="submit" name="action">Envoyer
-                            <i class="material-icons right">send</i>
-                        </button>
+                        <button class="modal-close waves-effect waves-light btn red accent-1 modal-trigger" type="submit" name="action">Valider</button>
                     </div>
                 </form>
             </div>
         </div>
+<?php include 'actionBtn.html'; ?>
+    
+        <div class="whiteSapce200"></div>
+        <!-- debut du footer -->
+         <?php include 'footer.html'; ?>
+        <!-- fin du footer -->
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> 
+        <script type="text/javascript" src="assets/js/materialize.min.js"></script>
+        <script type="text/javascript" src="assets/js/js.js"></script>
+    </body>
+</html>
